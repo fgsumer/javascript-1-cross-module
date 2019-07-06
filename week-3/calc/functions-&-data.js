9ab54b352978b948babb3f5e5daca1e39919b8ef
fgsumer <50924016+fgsumer@users.noreply.github.com>
2019-07-06 18:33:00 +0200

    Create functions-&-data.js

---
+++ week-3/calc/functions-&-data.js
@@ -0,0 +1,25 @@
+// data (variables)
+var a = 3;
+var b = 5;
+
+// functions
+
+    function print_vals (a,b) {
+        console.log(a, b);
+    }
+
+    function do_math (a, b, func) {
+        console.log(func(a, b));
+    }
+    function add (a, b) {
+      return a+b;
+    }
+    function subtract (a,b) {
+      return a-b;
+    }
+    function multiply (a,b) {
+      return a*b;
+    }
+    function divide (a,b) {
+      return a/b;
+    }
