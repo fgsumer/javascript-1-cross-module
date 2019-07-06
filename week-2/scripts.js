e0d005244a7e9b4373a8afac91a0b79e7428f361
fgsumer <50924016+fgsumer@users.noreply.github.com>
2019-07-06 11:48:41 +0200

    Update scripts.js

--- week-2/scripts.js
+++ week-2/scripts.js
@@ -1,5 +1,24 @@
 // data (variables)
+var projectObjects = {
+    name:"Adopt an Animal",
+    link:"https://www.adopteereendier.be/",
+    description:"an official website of the Flemish government that you can adopt a pet from a recognized animal shelter"};
+
+        
 
 
 
 // functions
+function print_deets (projectObjects){
+    for (let key in projectObjects) {
+        console.log(projectObjects[key]);
+    }
+    
+  }
+
+
+
+  let print = document.getElementById("printButton");
+    print.onclick = function () {
+    print_deets (projectObjects);
+}
