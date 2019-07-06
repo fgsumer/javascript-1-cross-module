5429b0f25e669fc3d4d99bbd52a0cdd77e9279d9
fgsumer <50924016+fgsumer@users.noreply.github.com>
2019-07-06 11:52:53 +0200

    Update functions-data.js

--- week-2/calc/functions-data.js
+++ week-2/calc/functions-data.js
@@ -1,8 +1,26 @@
 // data (variables)
-
-
+var a = 30;
+var b = 50;
 
 // functions
 
+    function print_vals(a,b) {
+        console.log(a, b);
+    }
 
-/* onclick's */
+    function do_math(num1, num2, func) {
+        console.log(func(num1, num2));
+    }
+    function add(a, b) {
+      return a+b;
+    }
+    function subtract(a,b) {
+      return a-b;
+    }
+    function multiply(a,b) {
+      return a*b;
+    }
+    function divide(a,b) {
+      return a/b;
+    }
+
