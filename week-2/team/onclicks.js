189cbd64db291ff316da67b560f32603597b9e33
fgsumer <50924016+fgsumer@users.noreply.github.com>
2019-07-06 11:51:06 +0200

    Update onclicks.js

--- week-2/team/onclicks.js
+++ week-2/team/onclicks.js
@@ -1,1 +1,16 @@
-onclicks.js
+
+var printTeamName = document.getElementById("nameofteam");
+printTeamName.onclick = function() {  
+                            print_team_name(a);
+                          }
+
+var printTeamMembers = document.getElementById("membersofteam");
+printTeamMembers.onclick = function() {    
+                            print_team_members (b);
+                          }
+
+var printIntroduction = document.getElementById("introductionforboth");
+printIntroduction.onclick = function() {    
+                            introduce (a,b);
+                          }
+
